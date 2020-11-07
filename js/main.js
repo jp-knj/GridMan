1e7115e4377280ec52947d98a69503d7cbf41991
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-11-07 14:44:49 +0900

    :sparkles: Display created Grid Items

--- js/main.js
+++ js/main.js
@@ -1,22 +1,73 @@
 //let cols = document.querySelector('#columns');
 let minCol = document.querySelector('#itemWidth');
+let gridColGap = document.querySelector('#gridColGap');
+let gridRowGap = document.querySelector('#gridRowGap');
+let unifyGap = document.querySelector('#unifyGap');
 let gridWrapper = document.querySelector('.js-grid');
-//let numOfCols = cols.value;
-let minColWidth = minCol.value;
+
+let minColWidth = 200;
+let gridColGapValue = 16;
+let gridRowGapValue = 16;
+
+let isUnify = false;
 
 minCol.addEventListener('input', function () {
-    minColWidth = this.value;
-    generateGridItems();
+
+  if (this.value < 0) {
+    return;
+  }
+
+  minColWidth = this.value;
+  generateGridItems();
+
 });
 
-// cols.addEventListener('input', function () {
-//     numOfCols = this.value;
-//     generateGridItems();
-// });
+gridColGap.addEventListener('input', function () {
+
+  gridColGapValue = this.value;
+  generateGridItems();
+
+});
+
+unifyGap.addEventListener('input', function () {
+
+  if (this.checked) {
+      gridColGapValue = 20;
+      gridRowGapValue = 20;
+
+      gridColGap.value = gridColGapValue;
+      gridRowGap.value = gridRowGapValue;
+
+      isUnify = true;
+
+      generateGridItems();
+  }
+});
 
 function generateGridItems() {
-    gridWrapper.style.gridTemplateColumns = `repeat(auto-fill, minmax(${minColWidth}px, 1fr))`;
-    console.log('Hello');
+
+  if (isUnify) {
+
+    // Get the current focused element
+    let currentActiveElem = document.activeElement;
+
+    if(currentActiveElem.getAttribute('id') == 'gridColGap') {
+        gridRowGapValue = gridColGapValue;
+    }
+
+    if(currentActiveElem.getAttribute('id') == 'gridRowGap') {
+        gridColGapValue = gridRowGapValue;
+    }
+
+    gridColGap.value = gridColGapValue;
+    gridRowGap.value = gridRowGapValue;
+
+  }
+
+  gridWrapper.style.gridTemplateColumns = `repeat(auto-fit, minmax(${minColWidth}px, 1fr))`;
+  gridWrapper.style.gridColumnGap = `${gridColGapValue}px`;
+  gridWrapper.style.gridRowGap = `${gridRowGapValue}px`;
+
 }
 
-//console.log(numOfCols);
+
